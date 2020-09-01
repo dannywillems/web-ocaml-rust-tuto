22c1dacee73f2d480e5dcce22d8e04815854045a
Danny Willems <be.danny.willems@gmail.com>
2020-09-01 18:32:48 +0200

    OCamlformat

--- 001-basic-mixed-hello-world/ocaml/src/print_hello.ml
+++ 001-basic-mixed-hello-world/ocaml/src/print_hello.ml
@@ -2,19 +2,22 @@
    non-bindings routines
 *)
 module Binding = struct
-    let print_hello_world () =
-      (* This is the equivalent of the following JavaScript code:
+  let print_hello_world () =
+    (* This is the equivalent of the following JavaScript code:
 
-         var f = require("rust-ocaml-hello-world");
-         f.print_hello_world();
-      *)
-      let open Js_of_ocaml in
-      let m = Js.Unsafe.js_expr {|require ("rust-ocaml-hello-world")|} in
-      ignore @@ Js.Unsafe.fun_call (Js.Unsafe.get m "print_hello_world") [|Js.Unsafe.inject ()|]
+       var f = require("rust-ocaml-hello-world");
+       f.print_hello_world();
+    *)
+    let open Js_of_ocaml in
+    let m = Js.Unsafe.js_expr {|require ("rust-ocaml-hello-world")|} in
+    ignore
+    @@ Js.Unsafe.fun_call
+         (Js.Unsafe.get m "print_hello_world")
+         [| Js.Unsafe.inject () |]
 end
 
 let () =
   (* We call first the Rust routine *)
-  Binding.print_hello_world ();
+  Binding.print_hello_world () ;
   (* Jsoo transformed this in a console.log *)
   print_string "Hello, World from OCaml"
