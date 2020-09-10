3f3e5fd17cb32d8316cc1510406494d4b0867306
Danny Willems <be.danny.willems@gmail.com>
2020-09-10 17:29:28 +0200

    Update with several write

--- 004-pointers/ocaml/src/lib.ml
+++ 004-pointers/ocaml/src/lib.ml
@@ -4,7 +4,10 @@ module Bindings = struct
   let rustc_bls12_381_fr_random required_module () =
     let open Js_of_ocaml in
     let open Js.Unsafe in
-    ignore @@ fun_call (get required_module "rustc_bls12_381_fr_random") [| inject () |];
+    ignore
+    @@ fun_call
+         (get required_module "rustc_bls12_381_fr_random")
+         [| inject () |] ;
     let memory = Jsoo_lib_rust_wasm.Memory.get_buffer required_module in
     let res = Jsoo_lib_rust_wasm.Memory.Buffer.slice memory 0 32 in
     Jsoo_lib_rust_wasm.Memory.Buffer.to_bytes res
@@ -12,7 +15,8 @@ module Bindings = struct
   let rustc_bls12_381_fr_one required_module () =
     let open Js_of_ocaml in
     let open Js.Unsafe in
-    ignore @@ fun_call (get required_module "rustc_bls12_381_fr_one") [| inject () |];
+    ignore
+    @@ fun_call (get required_module "rustc_bls12_381_fr_one") [| inject () |] ;
     let memory = Jsoo_lib_rust_wasm.Memory.get_buffer required_module in
     let res = Jsoo_lib_rust_wasm.Memory.Buffer.slice memory 0 32 in
     Jsoo_lib_rust_wasm.Memory.Buffer.to_bytes res
@@ -20,7 +24,9 @@ module Bindings = struct
   let rustc_bls12_381_fr_zero required_module () =
     let open Js_of_ocaml in
     let open Js.Unsafe in
-    ignore @@ fun_call (get required_module "rustc_bls12_381_fr_zero") [| inject () |];
+    ignore
+    @@ fun_call (get required_module "rustc_bls12_381_fr_zero") [| inject () |] ;
+    (* The value is gonna be in the first 32 bytes of the buffer *)
     let memory = Jsoo_lib_rust_wasm.Memory.get_buffer required_module in
     let res = Jsoo_lib_rust_wasm.Memory.Buffer.slice memory 0 32 in
     Jsoo_lib_rust_wasm.Memory.Buffer.to_bytes res
@@ -29,10 +35,28 @@ end
 let () =
   let open Js_of_ocaml.Js in
   let open Js_of_ocaml in
-  let p : (Jsoo_lib.ESModule.t, Unsafe.any) Jsoo_lib.Promise.t = Jsoo_lib.Promise.of_any_js (Unsafe.js_expr {| import ("rust-ocaml-pointers") |}) in
+  let p : (Jsoo_lib.ESModule.t, Unsafe.any) Jsoo_lib.Promise.t =
+    Jsoo_lib.Promise.of_any_js
+      (Unsafe.js_expr {| import ("rust-ocaml-pointers") |})
+  in
   let on_resolved m =
-    Firebug.console##log m;
+    Firebug.console##log m ;
     let random = Bindings.rustc_bls12_381_fr_random m () in
-    print_endline @@ String.concat " " (List.map (fun x -> string_of_int (int_of_char x)) (List.of_seq (Bytes.to_seq random)))
+    print_endline
+    @@ String.concat
+         " "
+         (List.map
+            (fun x -> string_of_int (int_of_char x))
+            (List.of_seq (Bytes.to_seq random))) ;
+    Jsoo_lib_rust_wasm.Memory.copy_in_buffer m random 0 32 32 ;
+    Firebug.console##log m ;
+    let random = Bindings.rustc_bls12_381_fr_random m () in
+    print_endline
+    @@ String.concat
+         " "
+         (List.map
+            (fun x -> string_of_int (int_of_char x))
+            (List.of_seq (Bytes.to_seq random))) ;
+    Firebug.console##log m
   in
   Jsoo_lib.Promise.then_bind ~on_resolved p
