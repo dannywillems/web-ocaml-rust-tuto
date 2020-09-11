54da6d7ded435535c356cec5c5602e6c5ae8b3d4
Danny Willems <be.danny.willems@gmail.com>
2020-09-11 11:31:12 +0200

    Success playing with pointers

--- 004-pointers/ocaml/src/lib.ml
+++ 004-pointers/ocaml/src/lib.ml
@@ -5,9 +5,7 @@ module Bindings = struct
     let open Js_of_ocaml in
     let open Js.Unsafe in
     ignore
-    @@ fun_call
-         (get required_module "rustc_bls12_381_fr_random")
-         [| inject () |] ;
+    @@ fun_call (get required_module "rustc_bls12_381_fr_random") [| inject 0 |] ;
     let memory = Jsoo_lib_rust_wasm.Memory.get_buffer required_module in
     let res = Jsoo_lib_rust_wasm.Memory.Buffer.slice memory 0 32 in
     Jsoo_lib_rust_wasm.Memory.Buffer.to_bytes res
@@ -16,7 +14,9 @@ module Bindings = struct
     let open Js_of_ocaml in
     let open Js.Unsafe in
     ignore
-    @@ fun_call (get required_module "rustc_bls12_381_fr_one") [| inject () |] ;
+    @@ fun_call
+         (get required_module "rustc_bls12_381_fr_one")
+         [| Js.Unsafe.inject 0 |] ;
     let memory = Jsoo_lib_rust_wasm.Memory.get_buffer required_module in
     let res = Jsoo_lib_rust_wasm.Memory.Buffer.slice memory 0 32 in
     Jsoo_lib_rust_wasm.Memory.Buffer.to_bytes res
@@ -30,7 +30,31 @@ module Bindings = struct
     let memory = Jsoo_lib_rust_wasm.Memory.get_buffer required_module in
     let res = Jsoo_lib_rust_wasm.Memory.Buffer.slice memory 0 32 in
     Jsoo_lib_rust_wasm.Memory.Buffer.to_bytes res
+
+  let rustc_bls12_381_fr_add required_module a b =
+    let open Js_of_ocaml in
+    let open Js.Unsafe in
+    Jsoo_lib_rust_wasm.Memory.copy_in_buffer required_module a 0 32 32 ;
+    Jsoo_lib_rust_wasm.Memory.copy_in_buffer required_module b 0 64 32 ;
+    ignore
+    @@ fun_call
+         (get required_module "rustc_bls12_381_fr_add")
+         [| inject 0; inject 32; inject 64 |] ;
+    (* The value is gonna be in the first 32 bytes of the buffer *)
+    let memory = Jsoo_lib_rust_wasm.Memory.get_buffer required_module in
+    let res = Jsoo_lib_rust_wasm.Memory.Buffer.slice memory 0 32 in
+    Jsoo_lib_rust_wasm.Memory.Buffer.to_bytes res
 end
+
+let print_fr_bytes_in_console a =
+  print_endline
+  @@ String.concat
+       " "
+       (List.map
+          (fun x -> string_of_int (int_of_char x))
+          (List.of_seq (Bytes.to_seq a)))
+
+let bytes_to_zarith b = Z.to_string (Z.of_bits (Bytes.to_string b))
 
 let () =
   let open Js_of_ocaml.Js in
@@ -40,23 +64,18 @@ let () =
       (Unsafe.js_expr {| import ("rust-ocaml-pointers") |})
   in
   let on_resolved m =
-    Firebug.console##log m ;
-    let random = Bindings.rustc_bls12_381_fr_random m () in
-    print_endline
-    @@ String.concat
-         " "
-         (List.map
-            (fun x -> string_of_int (int_of_char x))
-            (List.of_seq (Bytes.to_seq random))) ;
-    Jsoo_lib_rust_wasm.Memory.copy_in_buffer m random 0 32 32 ;
-    Firebug.console##log m ;
-    let random = Bindings.rustc_bls12_381_fr_random m () in
-    print_endline
-    @@ String.concat
-         " "
-         (List.map
-            (fun x -> string_of_int (int_of_char x))
-            (List.of_seq (Bytes.to_seq random))) ;
-    Firebug.console##log m
+    let a = Bindings.rustc_bls12_381_fr_random m () in
+    let b = Bindings.rustc_bls12_381_fr_random m () in
+    Firebug.console##log
+      Jsoo_lib_rust_wasm.Memory.(Buffer.to_any_js (get_buffer m)) ;
+    Firebug.console##log (bytes_to_zarith a) ;
+    Firebug.console##log (bytes_to_zarith b) ;
+    print_fr_bytes_in_console a ;
+    print_fr_bytes_in_console b ;
+    let sum = Bindings.rustc_bls12_381_fr_add m a b in
+    Firebug.console##log
+      Jsoo_lib_rust_wasm.Memory.(Buffer.to_any_js (get_buffer m)) ;
+    Firebug.console##log (bytes_to_zarith sum) ;
+    print_fr_bytes_in_console sum
   in
   Jsoo_lib.Promise.then_bind ~on_resolved p
--- 004-pointers/rust/src/lib.rs
+++ 004-pointers/rust/src/lib.rs
@@ -1,7 +1,7 @@
 use std::ptr;
 use wasm_bindgen::prelude::*;
 
-use ff::{Field, PrimeField, PrimeFieldRepr};
+use ff::{Field, PrimeField, PrimeFieldDecodingError, PrimeFieldRepr};
 use pairing::bls12_381;
 use pairing::bls12_381::Fr;
 
@@ -9,6 +9,12 @@ use rand::rngs::OsRng;
 
 const LENGTH_FR_BYTES: usize = 32;
 type c_uchar = u8;
+
+pub fn read_fr(from: &[c_uchar; LENGTH_FR_BYTES]) -> Result<Fr, PrimeFieldDecodingError> {
+    let mut f = <Fr as PrimeField>::Repr::default();
+    f.read_le(&from[..]).unwrap();
+    Fr::from_repr(f)
+}
 
 pub fn write_fr(buffer: *mut [c_uchar; LENGTH_FR_BYTES], element: Fr) {
     let buffer = unsafe { &mut *buffer };
@@ -39,4 +45,16 @@ pub fn rustc_bls12_381_fr_one(buffer: *mut [c_uchar; LENGTH_FR_BYTES]) {
 pub fn rustc_bls12_381_fr_random(buffer: *mut [c_uchar; LENGTH_FR_BYTES]) {
     let random_x = Fr::random(&mut OsRng);
     write_fr(buffer, random_x)
+}
+
+#[wasm_bindgen]
+pub extern "C" fn rustc_bls12_381_fr_add(
+    buffer: *mut [c_uchar; LENGTH_FR_BYTES],
+    x: *const [c_uchar; LENGTH_FR_BYTES],
+    y: *const [c_uchar; LENGTH_FR_BYTES],
+) {
+    let mut x = read_fr(unsafe { &*x }).unwrap();
+    let y = read_fr(unsafe { &*y }).unwrap();
+    x.add_assign(&y);
+    write_fr(buffer, x);
 }
